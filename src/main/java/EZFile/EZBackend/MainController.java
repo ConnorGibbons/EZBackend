08f850c1f06449203b11e825e0bf9487f36385a0
Connor Gibbons <connorgibbons@comcast.net>
2023-10-14 17:49:31 -0400

    Cleaned up tests, "File" cache for less querying

--- src/main/java/EZFile/EZBackend/MainController.java
+++ src/main/java/EZFile/EZBackend/MainController.java
@@ -1,5 +1,6 @@
 package EZFile.EZBackend;
 import java.util.*;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,59 +30,35 @@ public class MainController {
     @Autowired
     private ATTACH_PROPOSALRepository attach_proposalRepository;
 
-    @GetMapping("/test1")
-    public Iterable<PROJ_INFO> test1() {
-        return proj_infoRepository.findAll();
-    }
+    private List<File> allFiles;
 
-    @GetMapping("/test2")
-    public Iterable<PROJ_TYPE> test2() {
-        return proj_typeRepository.findAll();
-    }
-
-    @GetMapping("/test3")
-    public Iterable<PROPOSAL_INFO> test3() {
-        return proposal_infoRepository.findAll();
-    }
-
-    @GetMapping("/test4")
-    public Iterable<RES_INFO> test4(){
-        return res_infoRepository.findAll();
-    }
-
-    @GetMapping("/test5")
-    public Iterable<CUST_INFO> test5(){
-        return cust_infoRepository.findAll();
-    }
-
-    @GetMapping("/test6")
-    public Iterable<ATTACHMENT_FILE> test6(){
-        return attachment_fileRepository.findAll();
-    }
+    private Date refreshDate = new Date();
 
     @GetMapping("/files")
-    public Iterable<ATTACHMENT_FILE> files(@RequestParam(name = "fileType", required = false, defaultValue = "all") String fileType) {
-        if (fileType.equals("all")) {
-            return attachment_fileRepository.findAll();
+    public List<File> files(
+        @RequestParam(name = "fileExtension", required = false, defaultValue = "all") String fileExtension) {
+        if (allFiles == null || (diffInMinutes(refreshDate, new Date()) >= 5)) {
+            allFiles = genAllFiles(); // This might break if two requests come in simultaneously 
+            System.out.println("No file cache present or cache was outdates, generating one now.");
+        }
+
+        if (fileExtension.equals("all")) {
+            return allFiles;
         } else {
-            List<ATTACHMENT_FILE> matchingfiles = new ArrayList<>();
-            for (ATTACHMENT_FILE file : attachment_fileRepository.findAll()) {
-                if (file.getFILE_NAME().endsWith(fileType)) {
-                    matchingfiles.add(file);
-                }
-            }
-            return matchingfiles;
+            return allFiles.stream().filter(file -> file.getFileExtension().equals(fileExtension)).collect(Collectors.toList());
         }
     }
 
-    @GetMapping("/filesClassConstruction")
-    public List<File> filesClassConstruction() {
+    public List<File> genAllFiles() {
         List<File> files = new ArrayList<>();
         for (ATTACHMENT_FILE file : attachment_fileRepository.findAll()) {
             files.add(new File(file, cust_infoRepository, period_infoRepository, auc_infoRepository, proj_infoRepository, res_infoRepository));
         }
         return files;
-    }
+    }  
 
+    public Integer diffInMinutes(Date time1, Date time2) {
+        return (int) ((time2.getTime() - time1.getTime()) / (1000 * 60));
+    }
 
 }
