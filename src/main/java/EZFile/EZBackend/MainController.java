8902c9540179d279e806671a8400ad38809c2d54
Olujimi <olujimibrown@umass.edu>
2023-10-20 14:42:02 -0400

    added filtering by params

--- src/main/java/EZFile/EZBackend/MainController.java
+++ src/main/java/EZFile/EZBackend/MainController.java
@@ -1,7 +1,15 @@
 package EZFile.EZBackend;
-import java.util.*;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,18 +44,62 @@ public class MainController {
 
     @GetMapping("/files")
     public List<File> files(
-        @RequestParam(name = "fileExtension", required = false, defaultValue = "all") String fileExtension) {
-        if (allFiles == null || (diffInMinutes(refreshDate, new Date()) >= 5)) {
-            allFiles = genAllFiles(); // This might break if two requests come in simultaneously 
-            System.out.println("No file cache present or cache was outdates, generating one now.");
-        }
+        @RequestParam MultiValueMap<String, String> values) throws Exception {
 
-        if (fileExtension.equals("all")) {
-            return allFiles;
-        } else {
-            return allFiles.stream().filter(file -> file.getFileExtension().equals(fileExtension)).collect(Collectors.toList());
+        String[] validParameters = {"getFileName"};
+        List<BiFunction<List<File>, String, List<File>>> functionList = new ArrayList<>();
+
+        if (allFiles == null || (diffInMinutes(refreshDate, new Date()) >= 5)) { // If there is no cache or the cache is older than 5 minutes, rebuild the cache.
+            allFiles = genAllFiles(); // This might break if two requests come in simultaneously 
+            System.out.println("No file cache present or cache was outdated, generating one now.");
         }
+        List<File> resultFiles = allFiles;
+
+
+        //Function newFunc = (Object files) -> {return files.stream().filter(file -> file.getFileName().equals("Project docs.zip"));};
+        //List<Function> funcs = generateFunctionList(values);
+        //Function<List<File>, List<File>> newFunc = files ->  {return files.stream().filter(file -> file.getFileName().equals("Project docs.zip")).collect(Collectors.toList());};
+        
+        
+        BiFunction <List<File>,  Map<String, List<String>>, List<File>> newFunc = (files, fileArgs) -> {
+            List<File> t = files.stream().filter(file -> {
+                try {
+
+                    String filterCategory = fileArgs.keySet().toArray()[0].toString();
+                    Method m = file.getClass().getMethod(filterCategory);
+                    Object gname = m.invoke(file);
+                    System.out.println(gname);
+                } catch (Exception e) {
+
+                } finally {
+
+                }
+
+                return true;
+            }).collect(Collectors.toList());
+            return files;
+        };
+
+
+
+        for(String key : values.keySet()) {
+            Map<String, List<String>> filter = new HashMap<>();
+            filter.put(key, values.get(key));
+
+            resultFiles = newFunc.apply(resultFiles, filter);
+        };
+
+
+
+        //List<File> test = newfunc.apply(allFiles);
+        //System.out.println(test);
+        return allFiles;
     }
+
+
+
+
+
 
     public List<File> genAllFiles() {
         List<File> files = new ArrayList<>();
@@ -55,7 +107,7 @@ public class MainController {
             files.add(new File(file, cust_infoRepository, period_infoRepository, auc_infoRepository, proj_infoRepository, res_infoRepository));
         }
         return files;
-    }  
+    }
 
     public Integer diffInMinutes(Date time1, Date time2) {
         return (int) ((time2.getTime() - time1.getTime()) / (1000 * 60));
